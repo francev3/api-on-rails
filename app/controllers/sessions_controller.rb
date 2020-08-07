fbab0fe852086f890bd59576a139a0e970db39a3
Francisco Alvarado <franc.jadev@gmail.com>
2020-08-07 10:51:58 -0400

    added login and logout, models and seed for user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < Devise::SessionsController
+	include Devise::Controllers::Helpers
+	prepend_before_action :require_no_authentication, only: [:create]
+	respond_to :json
+
+	def create
+    resource = User.find_for_database_authentication(:email => params[:email])
+    return failure unless resource
+
+    if resource.valid_password?(params[:password])
+			sign_in(resource_name, resource)
+			yield resource if block_given? 
+
+      render :json=> {ok: true, token: current_token}
+      return
+    end
+    failure
+  end
+
+  def failure
+    return render json: { ok: false, errors: 'Correo electrónico o contraseña no válidos'}, :status => :unauthorized
+  end
+
+
+	private
+
+	def current_token
+			request.env['warden-jwt_auth.token']
+	end
+
+	def respond_to_on_destroy
+			head :no_content
+	end
+end
